052e7f37cd806531969c9ed50c295eb2c09481e2
Delta Student <satyaprakashroul324>
2025-04-12 00:13:55 +0530

    ready for deployment

--- backend/index.js
+++ backend/index.js
@@ -7,21 +7,15 @@ import userRoute from "./routes/user.route.js";
 import postRoute from "./routes/post.route.js";
 import messageRoute from "./routes/message.route.js";
 import { app,server } from "./socket/socket.js";
-// import path from "path";
+import path from "path";
 
 dotenv.config({});
 
 
 const PORT=process.env.PORT || 8000;
 
-// const __dirname=path.resolve();
+const __dirname=path.resolve();
 
-app.get("/",(req,res)=>{
-    return res.status(200).json({
-        message:"I'm coming from backend",
-        success:true
-    })
-})
 
 //middlewares
 app.use(express.json());
@@ -40,10 +34,10 @@ app.use("/api/v1/user",userRoute);
 app.use("/api/v1/post",postRoute);
 app.use("/api/v1/message",messageRoute);
 
-// app.use(express.static(path.join(__dirname, "/frontend/dist")));
-// app.get("*", (req,res)=>{
-//     res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-// })
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+app.get("*", (req,res)=>{
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+})
 
 server.listen(PORT,()=>{
     connectDB();
